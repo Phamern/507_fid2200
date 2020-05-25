7cab182e810cda78fc1f815459f5168db019c841
Eivind Madsen Pham <eivindmpham@gmail.com>
2020-05-25 12:37:16 +0200

    Testing API

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
+
 <script>
-	export let name;
+
+import { onMount } from 'svelte';
+
+const apiURL = "http://www.fruityvice.com/api/fruit/all";
+let data = [];
+
+	onMount(async function() {
+		const response = await fetch(apiURL);
+		data = await response.json();
+	});
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div>
+{#each data as item}
+	<div>
+		<p>{item.genus}</p>
+	</div>
+	{/each}
+</div>
